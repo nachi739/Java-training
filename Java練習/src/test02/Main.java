3e738fd9e3cc31324d39cca402261f3f762554e5
nachi739 <namiki081025@gmail.com>
2021-10-06 16:47:49 +0900

    test02

---
+++ Java練習/src/test02/Main.java
@@ -0,0 +1,17 @@
+package test02;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		String l = sc.nextLine();
+		String m = sc.nextLine();
+
+		System.out.println(s);
+		System.out.println(l);
+		System.out.println(m);
+		sc.close();
+	}
+}
