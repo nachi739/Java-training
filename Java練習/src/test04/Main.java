b6aca6b4ee4d495669fee2c986a6ece3499ef761
nachi739 <namiki081025@gmail.com>
2021-10-12 16:29:49 +0900

    test04

---
+++ Java練習/src/test04/Main.java
@@ -0,0 +1,14 @@
+package test04;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] strArray = sc.nextLine().split(" ");
+		System.out.println(strArray[0]);
+		System.out.println(strArray[1]);
+		System.out.println(strArray[2]);
+	}
+}
