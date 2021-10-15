82552735dd4c7b9794059a1d31178de2e05b08ac
nachi739 <namiki081025@gmail.com>
2021-10-15 17:27:38 +0900

    test09

---
+++ Java練習/src/test09/Main.java
@@ -0,0 +1,15 @@
+package test09;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 11; i++) {
+			System.out.print(i);
+			if (i < 10) {
+				System.out.print(" ");
+			} else {
+				System.out.println();
+			}
+		}
+	}
+}
