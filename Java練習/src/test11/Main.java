658068c52a87567d1fe28325f3a3f3a58af4f3ef
nachi739 <namiki081025@gmail.com>
2021-10-20 17:25:37 +0900

    test11

---
+++ Java練習/src/test11/Main.java
@@ -0,0 +1,20 @@
+package test11;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n == 1) {
+        	System.out.println(1);
+        } else {
+        	System.out.println(1);
+        	System.out.println(2);
+        }
+	}
+}
