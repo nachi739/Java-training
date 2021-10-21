197fcbdc7e8879999e1dcf79f78032ad424e2ed3
nachi739 <namiki081025@gmail.com>
2021-10-21 17:55:24 +0900

    test13

---
+++ Java練習/src/test13/Main.java
@@ -0,0 +1,30 @@
+package test13;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		 Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+
+	        if (n >= 1) {
+	        	System.out.println(1);
+	        }
+	        if (n >= 2) {
+	        	System.out.println(2);
+	        }
+	        if (n >= 3) {
+	        	System.out.println(3);
+	        }
+	        if (n >= 4) {
+	        	System.out.println(4);
+	        }
+	        if (n >= 5) {
+	        	System.out.println(5);
+	        }
+
+	        sc.close();
+	}
+}
