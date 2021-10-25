60f1867a79535f8501796fc1d7f7c27fa322840a
nachi739 <namiki081025@gmail.com>
2021-10-25 19:10:10 +0900

    test18

---
+++ Java練習/src/test18/Main.java
@@ -0,0 +1,19 @@
+package test18;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			String name = sc.next();
+			int age = sc.nextInt();
+			age+= 1;
+			System.out.println(name + " " + age);
+		}
+
+	}
+}
