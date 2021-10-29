c4e20b61300b3868d213c15b14bc014c1be3e42f
Yuichi Namiki <namiki081025@gmail.com>
2021-10-29 21:19:22 +0900

    test25

--- Java練習/src/test25/Main.java
+++ Java練習/src/test25/Main.java
@@ -1,9 +1,45 @@
 package test25;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		//test
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			String start = sc.next();
+			int h = sc.nextInt();
+			int m = sc.nextInt();
+			
+			int startH = Integer.parseInt(start.substring(0,2));
+			int startM = Integer.parseInt(start.substring(3,5));
+			
+			int ah = startH + h;
+			int am = startM + m;
+			
+			if (am >= 60) {
+				ah += 1;
+				am -= 60;
+			}
+			
+			if (ah >= 24) {
+				ah -= 24;
+			}
+			
+			String endH = Integer.toString(ah);
+			String endM = Integer.toString(am);
+			
+			if (endH.length() == 1) {
+				endH = "0" + endH;
+			}
+			
+			if (endM.length() == 1) {
+				endM = "0" + endM;
+			}
+			
+			System.out.println(endH + ":" + endM);
+		}
 	}
-
 }
