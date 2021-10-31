21677b4b641143919d666781caadd7a59329f186
Yuichi Namiki <namiki081025@gmail.com>
2021-10-31 20:50:09 +0900

    test26

--- Java練習/src/test26/Main.java
+++ Java練習/src/test26/Main.java
@@ -1,8 +1,21 @@
 package test26;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int d = 0;
+		
+		for (int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			
+			if (a % 3 == 0) {
+				d++;
+			}
+		}
+		System.out.print(d);
 	}
 }
