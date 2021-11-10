c8fae4a67e46c8c86cf5cef9a21936d9bc5af3aa
nachi739 <namiki081025@gmail.com>
2021-11-10 16:33:30 +0900

    test40

---
+++ Java練習/src/test40/Main.java
@@ -0,0 +1,15 @@
+package test40;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        String n = sc.next();
+        int n2 = sc.nextInt();
+
+        System.out.println(n1 + n2);
+    }
+}
